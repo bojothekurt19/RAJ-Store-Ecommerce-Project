342d96f425b87853986913d73a1d2ffe8da526ca
Kurt Cyryll Cubelo <kurtcubelo@gmail.com>
2024-04-26 08:27:17 +0800

    Add, Nav bar and product lay out. Add product data.

---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: number
+  name: string
+  image: string
+  category: string
+  brand: string
+  price: number
+  stockCount: number
+  description: string
+  rating: number
+  numberOfReviews: number
+}
