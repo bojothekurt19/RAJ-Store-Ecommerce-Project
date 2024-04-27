b9f56e062c48f115ad5af41d5abf031f441b2d4d
Kurt Cyryll Cubelo <kurtcubelo@gmail.com>
2024-04-27 10:57:54 +0800

    Setup backend with node server.

---
+++ backend/build/data.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sampleProduct = void 0;
+exports.sampleProduct = [
+    {
+        id: 1,
+        name: 'Dishwasher',
+        image: '../productImages/appliances/dishwasher.jpg',
+        category: 'Appliances',
+        brand: 'Samsung',
+        price: 8000,
+        stockCount: 51,
+        description: ' A time-saving appliance that efficiently cleans dishes, utensils, and cookware using hot water and detergent.',
+        rating: 4,
+        numberOfReviews: 0,
+    },
+    {
+        id: 2,
+        name: 'Oven',
+        image: '../productImages/appliances/oven.jpeg',
+        category: 'Appliances',
+        brand: 'Samsung',
+        price: 8000,
+        stockCount: 20,
+        description: ' A kitchen appliance used for baking, roasting, and heating food by circulating hot air within an enclosed space. ',
+        rating: 5,
+        numberOfReviews: 5,
+    },
+    {
+        id: 3,
+        name: 'Refrigerator',
+        image: '../productImages/appliances/ref.jpeg',
+        category: 'Appliances',
+        brand: 'Hanabishi',
+        price: 60000,
+        stockCount: 10,
+        description: ' An essential kitchen appliance that keeps food and beverages cool and fresh by maintaining a low temperature.',
+        rating: 5,
+        numberOfReviews: 2,
+    },
+];
