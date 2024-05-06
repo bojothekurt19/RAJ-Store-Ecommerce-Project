605103873642687710f1b91566621d091a8963ee
Kurt Cyryll Cubelo <kurtcubelo@gmail.com>
2024-05-06 18:48:15 +0800

    Fetch data using react query.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,17 +6,18 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
-
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home.tsx'
 import ProductPage from './pages/ProductPage.tsx'
-import axios from 'axios'
+// import axios from 'axios'
 import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,11 +29,15 @@ const router = createBrowserRouter(
     </Route>
   )
 )
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 )
